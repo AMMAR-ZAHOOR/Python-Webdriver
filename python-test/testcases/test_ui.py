aa44f2979973666531ade9572018c3005972a063
AMMAR-ZAHOOR <77923105+AMMAR-ZAHOOR@users.noreply.github.com>
2024-08-09 02:02:44 +0500

    Selenium Python
    Selenium Test

---
+++ python-test/testcases/test_ui.py
@@ -0,0 +1,22 @@
+import unittest
+
+from webdriver import Driver
+from values import strings
+from pageobjects.homescreen import HomeScreen
+
+class verifyMessageTest(unittest.TestCase):
+  
+  def setUp(self):
+    self.driver = Driver()
+    self.driver.navigate(strings.base_url)
+    
+  def test_displayedMessage(self):
+    home_screen = HomeScreen(self.driver)    
+    home_screen.validate_message_displayed()
+        
+  def tearDown(self):
+    self.driver.instance.quit()     
+   
+
+if __name__ == '__main__':
+  unittest.main()    
